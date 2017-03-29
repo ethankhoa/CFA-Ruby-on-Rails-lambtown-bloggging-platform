69ed01daae16dc3e6b08e346786837a9699c9577
ethankhoa <ethan.huynh@gmail.com>
2017-03-29 11:02:56 +1100

    added slug candidates

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,6 +1,13 @@
 class Post < ApplicationRecord
   extend FriendlyId
-  friendly_id :title, use: :slugged
+  friendly_id :slug_candidates, use: :slugged
   belongs_to :user
   has_many :comments
+
+  def slug_candidates
+    [
+      :title,
+      [:title, :content]
+    ]
+
 end
