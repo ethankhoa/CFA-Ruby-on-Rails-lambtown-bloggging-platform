655c4cdc263c75add8f5123b475fecaa3d53fda4
ethankhoa <ethan.huynh@gmail.com>
2017-03-31 11:59:59 +1100

    basic install of rolify

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,21 @@
 class User < ApplicationRecord
-  rolify
+  after_create :assign_default_role
+  rolify :before_add => :before_add_method
+
+  def before_add_method(role)
+    # do something before it gets added
+  end
+
+  def assign_default_role
+  self.add_role(:newuser) if self.roles.blank?
+  end
+
+  def set_admin_user
+    user = User.find(1)
+    user.add_role :admin
+  end
+
+
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
