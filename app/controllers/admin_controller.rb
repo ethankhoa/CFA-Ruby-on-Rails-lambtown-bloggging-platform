f32c90d82557ae09b4995895cc36499e644bd0c2
ethankhoa <ethan.huynh@gmail.com>
2017-03-31 11:19:08 +1100

    add authorize user method

--- app/controllers/admin_controller.rb
+++ app/controllers/admin_controller.rb
@@ -1,6 +1,14 @@
 class AdminController < ApplicationController
+  before_action :authorize_user, only: :index
   def index
     @users = User.all
     @posts = Post.all
   end
+
+  def authorize_user
+    if current_user.user_type != "admin"
+      redirect_to root_path, notice: 'You shall not pass'
+    end
+  end
+
 end
